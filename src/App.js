c539a4fd2425c7aadd3558675898cc742a667fbf
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2021-08-18 08:27:56 -0300

    add: interval component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Card from "./components/Card";
+import Interval from "./components/Interval";
 
 function App() {
   return (
     <div className="App">
       <h1>React redux simple exercise</h1>
       <div className='line'>
-        <Card title="Card 1" red>X</Card>
-        <Card title="Card 2" green>X</Card>
+        <Interval title='Number interval'></Interval>
       </div>
       <div className='line'>
+        <Card title="Card 2" green>X</Card>
         <Card title="Card 3" blue>Y</Card>
         <Card title="Card 4" purple>Y</Card>
       </div>
---
+++ src/components/Interval.jsx
@@ -0,0 +1,21 @@
+import "./Interval.css"
+import React from "react"
+import Card from "./Card";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props => {
+  return (
+    <Card title='Number interval' red>
+      <div className='Interval'>
+        <span>
+          <strong>Minimum</strong>
+          <input type="number" value={0} />
+        </span>
+        <span>
+          <strong>Maximum</strong>
+          <input type="number" value={10} readOnly />
+        </span>
+      </div>
+    </Card>
+  )
+}
